e5da89cbf22a5df89802cf6a2263bfbfbcde157c
jerometecson-main <zxctest8@zxcstream.xyz>
2026-06-23 21:10:38 +0800

    zxc

--- lib/allowed-referers.ts
+++ lib/allowed-referers.ts
@@ -1,24 +1,24 @@
 const ALLOWED_REFERERS = [
-  "http://localhost:3000/",
+  // "http://localhost:3000/",
   "https://zxcprime.site/",
   "https://www.zxcprime.site/",
   "https://zxcstream.xyz/",
   "https://v.zxcstream.xyz/",
+  "https://z.zxcstream.xyz/",
   "https://embed.zxcstream.xyz/",
-  "https://player.zxcstream.xyz/",
   "https://cdn.zxcstream.xyz/",
   "https://www.zxcstream.xyz/",
 ];
 
 export const ALLOWED_ORIGINS = [
-  "http://localhost:3000",
+  // "http://localhost:3000",
   "https://zxcprime.site",
   "https://www.zxcprime.site",
   "https://zxcstream.xyz",
   "https://www.zxcstream.xyz",
   "https://v.zxcstream.xyz",
+  "https://z.zxcstream.xyz",
   "https://embed.zxcstream.xyz",
-  "https://player.zxcstream.xyz",
   "https://cdn.zxcstream.xyz",
 ];
 export function isValidReferer(referer: string): boolean {
