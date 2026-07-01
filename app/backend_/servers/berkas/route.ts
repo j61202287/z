d4a68073353518248ee94cdfb34cb0459da43d07
jerometecson-main <zxctest8@zxcstream.xyz>
2026-07-02 02:25:29 +0800

    mor eworkers

--- app/backend_/servers/berkas/route.ts
+++ app/backend_/servers/berkas/route.ts
@@ -15,6 +15,8 @@ const supabase = createClient(
 );
 
 const PROXY_WORKERS = [
+  "https://berkas-test039.test039.workers.dev/",
+  "https://berkas.test038.workers.dev/",
   "https://berkas.test032.workers.dev/",
   "https://berkas.test034.workers.dev/",
   "https://berkas.test035.workers.dev/",
